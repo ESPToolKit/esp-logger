71f4d517cdbd00f2fc1ab8ed51d72a2ae6bbd0e4
zekageri <drsedate@gmail.com>
2025-09-17 15:22:39 +0200

    cmake cache problem

---
+++ build_prev_runner/test/CMakeFiles/logger_tests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/logger_tests.dir/logger_test_stubs.cpp.o"
+  "CMakeFiles/logger_tests.dir/logger_test_stubs.cpp.o.d"
+  "CMakeFiles/logger_tests.dir/logger_tests.cpp.o"
+  "CMakeFiles/logger_tests.dir/logger_tests.cpp.o.d"
+  "logger_tests"
+  "logger_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/logger_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
