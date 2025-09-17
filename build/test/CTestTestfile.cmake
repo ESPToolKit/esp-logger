7e0063c83a8bfb8510cf5b57265700f41919d931
zekageri <drsedate@gmail.com>
2025-09-17 15:27:36 +0200

    cmake cache problem fix?

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zekageri/projects/ESPToolKit/libs/esp-logger/test
-# Build directory: /home/zekageri/projects/ESPToolKit/libs/esp-logger/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(logger_tests "/home/zekageri/projects/ESPToolKit/libs/esp-logger/build/test/logger_tests")
-set_tests_properties(logger_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/zekageri/projects/ESPToolKit/libs/esp-logger/test/CMakeLists.txt;32;add_test;/home/zekageri/projects/ESPToolKit/libs/esp-logger/test/CMakeLists.txt;0;")
--- build_prev_runner/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zekageri/projects/ESPToolKit/libs/esp-logger
-# Build directory: /home/zekageri/projects/ESPToolKit/libs/esp-logger/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
